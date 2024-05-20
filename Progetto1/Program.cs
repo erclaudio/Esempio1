404ebee581e2a9e57c5cc45732602ce8934d3f3b
erclaudio <erfattone@gmail.com>
2024-05-20 11:05:33 +0100

    second commit

--- Progetto1/Program.cs
+++ Progetto1/Program.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// First comment
+
 namespace Progetto1
 {
     class Program
     {
-        static void Main1(string[] args)
+        static void Main(string[] args)
         {
         }
     }
