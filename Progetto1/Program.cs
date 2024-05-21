708bb8dffce9e56199914e99431e2eaf034b14cc
erclaudio <erfattone@gmail.com>
2024-05-21 10:11:08 +0100

    Some tests

--- Progetto1/Program.cs
+++ Progetto1/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿// include libraries (classes/namespaces)
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//declaring namespace)
 namespace Progetto1
 {
+       //declaring class
     class Program
     {
+        //declaring method
         static void Main(string[] args)
         {
+            string name;
+            //instructions
+            Console.WriteLine("Welcome in C#!, This script takes your name and prints it");
+            Console.WriteLine("...");
+            Console.Write("What's your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Ciao " + name);
+            Console.Read();
+
         }
     }
 }
